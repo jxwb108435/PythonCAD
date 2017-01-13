ea3adc7acb22c2fd4794cbe7a49df316e036c69a
Seaman <lin041012b@hotmail.com>
2017-01-14 00:44:04 +0800

    Create VersionInfo.vb

---
+++ VersionInfo.vb
@@ -0,0 +1,11 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+
+<Assembly: AssemblyCompany("Autodesk, Inc.")> 
+
+<Assembly: AssemblyCopyright("©Autodesk, Inc. 1998-2016. All rights reserved.")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyVersion("11.0.659.0")> 
+<Assembly: AssemblyFileVersion("11.0.659.0")> 
